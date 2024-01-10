c3272b7bcfc662b6ae7f16d78a23caa49e4b2597
Abdullah Mustapha <a41622100@gmail.com>
2024-01-10 20:53:11 +0000

    Added the deafult font

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -18,6 +18,8 @@ export default {
     background-color: #333;
     overflow: hidden;
     font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 
 .header-bar a {
