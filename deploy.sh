2ddfa7fc2b21b5624ae2e2b5ae516682549a3fb1
Abdullah Mustapha <a41622100@gmail.com>
2024-01-29 20:52:15 +0000

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # abort on errors
 set -e
 
-# build
+# # build
 npm run build
 
 # navigate into the build output directory
@@ -12,9 +12,11 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+#git init
 git add -A
 git commit -m 'deploy'
+
+git branch -m Main
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
